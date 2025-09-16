e4a5c4e19a09c037ba845ede6855a56b284e03c1
Janeirungu01 <jwirungu96@gmail.com>
2025-09-16 15:23:16 +0300

    Redirect user to dashboard after successful authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { authguardGuard } from './authguard-guard';
 import { Login } from './login/login';
-import { Navbar } from './navbar/navbar';
+
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Navbar, canActivate: [authguardGuard] },
+  { path: 'dashboard', component: Dashboard, canActivate: [authguardGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
