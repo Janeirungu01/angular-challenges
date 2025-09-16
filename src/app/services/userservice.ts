8fc66ded1d4a49c1bb3e6efa5f65070e4a03f7ea
Janeirungu01 <jwirungu96@gmail.com>
2025-09-16 16:25:14 +0300

    add users component and service

---
+++ src/app/services/userservice.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface User{
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Userservice {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient){}
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.apiUrl);
+
+  }
+  
+}
