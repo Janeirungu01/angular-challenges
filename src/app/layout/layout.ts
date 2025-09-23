5920e640c4ab75c4fe3b5fc367801604829c1a27
Janeirungu01 <jwirungu96@gmail.com>
2025-09-23 17:20:14 +0300

    create layout component that has a reusable sidebar and navbar

---
+++ src/app/layout/layout.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class Layout {
+   pageTitle: string = '';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.activatedRoute.firstChild;
+          while (route?.firstChild) {
+            route = route.firstChild;
+          }
+          return route?.snapshot.data['title'] || 'Dashboard';
+        })
+      )
+      .subscribe(title => {
+        this.pageTitle = title;
+      });
+  }
+
+}
